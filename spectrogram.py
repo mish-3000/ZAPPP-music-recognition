15abd427d8b239428912d24efa70b6b70ffd00ef
mish-3000 <mishty3000@gmail.com>
2026-08-13 09:45:51 +0530

    changed window and converted Sxx to decibels

--- spectrogram.py
+++ spectrogram.py
@@ -1,7 +1,7 @@
 from mp3_to_wav import wavFilesPaths
 from scipy.io import wavfile
 from scipy.signal import spectrogram
-from scipy.signal.windows import gaussian
+from scipy.signal.windows import hann
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,16 +22,16 @@ def show_spectrogram(waveFilesPaths):
         elif mono.dtype==np.int32:
             mono = mono/2147483648
         
-        window_dur=2
-        step_dur = window_dur / np.sqrt(np.pi) / 8
+        window_dur=0.09
+        step_dur = window_dur / 2
         step_samples = int(round(step_dur * sampleRate))
         window_samples = int(round(window_dur * sampleRate))
-        window_sigma=(window_samples+1)/6
-        window = gaussian(window_samples, window_sigma, sym=False)
+        window = hann(window_samples, sym=False)
         overlap = window_samples - step_samples
         freq, time, Sxx = spectrogram(mono, fs=sampleRate, window=window, nperseg=window_samples, noverlap=overlap)
+        melxx=np.where(Sxx>0, 10*np.log10(Sxx), -100)
         plt.figure()
-        plt.pcolormesh(time, freq, Sxx, shading='gouraud')
+        plt.pcolormesh(time[:100], freq, melxx[:, :100], shading='gouraud')
         plt.title('Spectrogram')
         plt.xlabel('Time (s)')
         plt.ylabel('Frequency (Hz)')
