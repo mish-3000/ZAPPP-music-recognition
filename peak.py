91fbcf03e6b4e7c44d564af5d99292d8831b2418
mish-3000 <mishty3000@gmail.com>
2026-08-18 02:05:10 +0530

    the peak infos for all songs are stored as a dictionary with the key as file name

--- peak.py
+++ peak.py
@@ -8,43 +8,53 @@ minFreqBin=23 ;'''approx 250Hz'''
 maxPeaksPerTimeframe=3
 minDB=-50
 def get_peaks(minFreqBin, maxPeaksPerTimeframe, minDB):
+    allPeaks={}
     for file in wavFilesPaths:
         freq, time, melxx = get_Sxx(file)
-        input_melxx=np.where(melxx[minFreqBin:,:]>minDB, melxx[minFreqBin:,:], -np.inf)
-       
-        '''only consider frequencies above minFreqBin and power above minDB. powers below minDB are set to -inf so they will never be considered as peaks'''
+        input_melxx=np.where(melxx[minFreqBin:,:]>minDB, melxx[minFreqBin:,:], -np.inf); '''only consider frequencies above minFreqBin and power above minDB. powers below minDB are set to -inf so they will never be considered as peaks'''
 
-        Pxx=ndimage.maximum_filter(input_melxx, size=5, mode='reflect')
-        '''creating a 2d array of the same shape as input_melxx where each element is the maximum value in a 5x5 neighborhood around the corresponding element in input_melxx.'''
+        Pxx=ndimage.maximum_filter(input_melxx, size=5, mode='reflect'); '''creating a 2d array of the same shape as input_melxx where each element is the maximum value in a 5x5 neighborhood around the corresponding element in input_melxx.'''
 
-        PxxFinalIndices=np.where(Pxx==input_melxx)
-        '''getting indices only where the local max is the original element itself. i.e. only local maximas survive in noise'''
+        PxxFinalIndices=np.where(Pxx==input_melxx);  '''getting indices only where the local max is the original element itself. i.e. only local maximas survive in noise'''
 
-        for i in range(Pxx.shape[1]):
-            if i in PxxFinalIndices[1]:
-                dictValueIndices=np.where(PxxFinalIndices[1]==i)[0] 
-                dictValues=Pxx[PxxFinalIndices[0][dictValueIndices], PxxFinalIndices[1][dictValueIndices]]
-                if len(dictValues)>maxPeaksPerTimeframe:
-                    dictValuesSorted=dictValues[np.argsort(dictValues)[-maxPeaksPerTimeframe:][::-1]]
-                else:
-                    dictValuesSorted=dictValues[np.argsort(dictValues)[::-1]]
-                '''dictKey=str(PxxFinalIndices[0][dictValueIndices][list(range(len(dictValuesSorted)))] )+'_'+str(i)'''
-                if -np.inf in dictValuesSorted:
-                    dictValuesSorted=dictValuesSorted[np.where(dictValuesSorted!=-np.inf)]
-                    '''dictKey=str(PxxFinalIndices[0][dictValueIndices][list(range(len(dictValuesSorted)))] )+'_'+str(i)'''
-                '''-inf can be considered as a peak if there are no other values in that neighborhood. so if -inf is present in the list of peaks, remove it and update the dictKey accordingly.'''
+        def get_peaks_for_file():
+            for i in range(Pxx.shape[1]):
+                if i in PxxFinalIndices[1]:
+                    dictValueIndices=np.where(PxxFinalIndices[1]==i)[0]; '''storing all indices where there is a peak for this particular time bin'''
+                    dictValues=Pxx[PxxFinalIndices[0][dictValueIndices], PxxFinalIndices[1][dictValueIndices]]
+                    if np.all(dictValues == -np.inf):
+                        continue; '''if all peaks are -infinity skip this iteration'''
+                    if len(dictValues)>maxPeaksPerTimeframe:
+                        sortedIndices=np.argsort(dictValues)[-maxPeaksPerTimeframe:][::-1]
+                        dictValuesSorted=dictValues[sortedIndices]
 
-                yield dictKey, dictValuesSorted
-        '''for every timeframe column, if any peaks exist, get indices of those peaks. if no. of peaks is more than maxPeaksPerTimeframe, sort the peaks in descending order and return only the top maxPeaksPerTimeframe peaks. else return all peaks sorted in descending order.'''      
+                    else:
+                        sortedIndices=np.argsort(dictValues)[::-1]
+                        dictValuesSorted=dictValues[sortedIndices]
+                    
+                    freqTimeArr=[]; '''creating array of tuples of freq and time for which there ia a peak in this time bin'''
+                    for j in sortedIndices:
+                        freqTimeTuple1=(freq[PxxFinalIndices[0][dictValueIndices[j]]+minFreqBin],time[i])
+                        freqTimeArr.append(freqTimeTuple1)
+                
+                
+                    if -np.inf in dictValuesSorted:
+                        indicesWithoutNegInf=np.where(dictValuesSorted!=-np.inf)[0]; '''-inf can be considered as a peak if there were no other higher values in its neighbourhood, so excluding it'''
+                        dictValuesSorted=dictValuesSorted[indicesWithoutNegInf]
+                    
+                        freqTimeArr=[]
+                        for j in indicesWithoutNegInf:
+                            freqTimeTuple2=(freq[PxxFinalIndices[0][dictValueIndices[sortedIndices][j]]+minFreqBin],time[i])
+                            freqTimeArr.append(freqTimeTuple2)
+                    for j in range(len(dictValuesSorted)):  
+                        dictkeyTuple=freqTimeArr[j] ; '''for every peak in this time bin, a freq, time tuple and the peak value is yielded'''
+                        yield dictkeyTuple, dictValuesSorted[j]
+        peaksDict={}; '''for all time bins and all their respective peaks, freq, time tuple and peak value is yielded and stored in a dictionary with the key as tuple'''
+        for dictKeyTuple, dictValuesSorted in get_peaks_for_file():
+            peaksDict.update({dictKeyTuple:dictValuesSorted}) 
+        allPeaks[file]=peaksDict ; '''for all the n-songs, n dictionaries are created with the file name as key that contain dictionary of peak infos of that song'''            
+    return allPeaks
 
-def get_peaks_Dict():
-    for dictKey, dictValuesSorted in get_peaks(minFreqBin, maxPeaksPerTimeframe, minDB):
-        peaksDict={dictKey: dictValuesSorted}
-        yield peaksDict
 
-peaksDictArray=[]
+        
 
-
-for peaksDict in get_peaks_Dict():
-    peaksDictArray.append(peaksDict)
-'''for every timeframe column where peaks exist, there will be a dictionary with key as time and freq bin reference and value as a list of peak values in descending order.'''
--- spectrogram.py
+++ spectrogram.py
@@ -6,32 +6,32 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import wavio
-def get_Sxx(waveFilesPaths):
-    for file in waveFilesPaths:
-        wavData = wavio.read(file)
-        sampleRate = wavData.rate
-        signal = wavData.data; '''returns a 2d array always, even if the audio is mono'''
-        if signal.shape[1] == 2:
-            mono=signal.mean(axis=1)
-        else:
-            mono=signal.reshape(-1)
-        if mono.dtype==np.uint8:
-            mono =(mono-128)/128
-        elif mono.dtype==np.int16:
-            mono = mono/32768
-        elif mono.dtype==np.int32:
-            mono = mono/2147483648
+def get_Sxx(file):
+    
+    wavData = wavio.read(file)
+    sampleRate = wavData.rate
+    signal = wavData.data; '''returns a 2d array always, even if the audio is mono'''
+    if signal.shape[1] == 2:
+        mono=signal.mean(axis=1)
+    else:
+        mono=signal.reshape(-1)
+    if mono.dtype==np.uint8:
+        mono =(mono-128)/128
+    elif mono.dtype==np.int16:
+        mono = mono/32768
+    elif mono.dtype==np.int32:
+        mono = mono/2147483648
         
-        window_dur=0.09
-        step_dur = window_dur / 2
-        step_samples = int(round(step_dur * sampleRate))
-        window_samples = int(round(window_dur * sampleRate))
-        window = hann(window_samples, sym=False)
-        overlap = window_samples - step_samples
-        freq, time, Sxx = spectrogram(mono, fs=sampleRate, window=window, nperseg=window_samples, noverlap=overlap)
-        melxx=np.where(Sxx>0, 10*np.log10(Sxx), -100)
+    window_dur=0.09
+    step_dur = window_dur / 2
+    step_samples = int(round(step_dur * sampleRate))
+    window_samples = int(round(window_dur * sampleRate))
+    window = hann(window_samples, sym=False)
+    overlap = window_samples - step_samples
+    freq, time, Sxx = spectrogram(mono, fs=sampleRate, window=window, nperseg=window_samples, noverlap=overlap)
+    melxx=np.where(Sxx>0, 10*np.log10(Sxx), -100)
         
-        return freq, time, melxx
+    return freq, time, melxx
 
 def show_spectrogram(freq, time, melxx):
         
